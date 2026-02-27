ea5b262610519f37f55dc4506020fbc3237754fa
Nicolás Ortega Froysa <nicolas@ortegas.org>
2026-02-27 14:30:11 +0100

    Remove public interface to set defaultPoints.
    This will be set in the imported files.

--- src/main/java/net/themusicinnoise/vaccalc/PointEngine.java
+++ src/main/java/net/themusicinnoise/vaccalc/PointEngine.java
@@ -9,8 +9,6 @@ public class PointEngine {
         defaultPoints = 1.0;
     }
 
-    public void setDefaultPoints(float defaultPoints) { this.defaultPoints = defaultPoints; }
-
     public double getPointsOfDay(LocalDate date) {
         return defaultPoints;
     }
